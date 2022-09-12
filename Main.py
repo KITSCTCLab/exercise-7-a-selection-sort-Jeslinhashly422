a3b02e6e512decf80da4a325f6d2d904643c7049
Jeslinhashly422 <109952248+Jeslinhashly422@users.noreply.github.com>
2022-09-12 14:59:27 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
+     for step in range(size):
+        min_idx = step
+
+        for i in range(step + 1, size):
+
+            if array[i] < array[min_idx]:
+                min_idx = i
+         
+        # put min at the correct position
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
   # Write your code here
 
 # Do not change the following code
